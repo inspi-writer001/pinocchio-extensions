61ecf9636ef9cd352967f26e1fe6c4d26cf1ccad
inspiration_gx <conservedinnovation@gmail.com>
2026-01-28 18:36:47 +0100

    fix: cleaning up cpi_guard pr

--- programs/token-2022/src/instructions/extensions/mod.rs
+++ programs/token-2022/src/instructions/extensions/mod.rs
@@ -1,11 +1,9 @@
 pub mod cpi_guard;
 pub mod memo_transfer;
-pub mod pausable;
 
 #[repr(u8)]
 #[non_exhaustive]
 pub enum ExtensionDiscriminator {
     MemoTransfer = 30,
-    Pausable = 44,
     CpiGuard = 34,
 }
--- programs/token-2022/src/instructions/extensions/pausable/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod initialize;
-pub mod pause;
-pub mod resume;
-
-pub use initialize::*;
-pub use pause::*;
-pub use resume::*;
